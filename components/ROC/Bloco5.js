510437bda10827fc55c8ac11e564525a24ee4954
Matheus Lasserre <matheuslasserre3@gmail.com>
2021-06-01 04:23:17 -0300

    Bloco 6 iniciado. Falta começar a criar o componente dos meses

--- components/ROC/Bloco5.js
+++ components/ROC/Bloco5.js
@@ -4,7 +4,13 @@ import B5Style from '../../styles/Bloco5.module.css'
 function Bloco5() {
     return (
         <div>
-            <div className={B5Style.bloco5Background}></div>
+            <div className={B5Style.contentWrap}>
+                <div className={B5Style.bloco5Background}>
+                    <p className={B5Style.headText}>Como o curso irá funcionar?</p>
+                    <p className={B5Style.bodyText}>Mensalmente, iremos trabalhar temas que perpassam aspectos fundamentais da vida humana e refletiremos a partir do poder artístico do cinema para posteriormente adentrarmos no clássico literário escolhido.</p>
+                    <p className={B5Style.bodyText}>Ou seja, <span className={B5Style.underline}>teremos um filme e um clássico da literatura analisados</span> todo mês, a partir das perspectivas temáticas da vida humana que se seguem:</p>
+                </div>
+            </div>
         </div>
     )
 }
