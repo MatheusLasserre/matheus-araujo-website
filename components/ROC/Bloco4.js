6315cfa1bc6c9b067fb389e0202b430331b04fbc
Matheus Lasserre <matheuslasserre3@gmail.com>
2021-06-01 01:15:08 -0300

    Texto do bloco 4 feito. Falta componente dos cards

--- components/ROC/Bloco4.js
+++ components/ROC/Bloco4.js
@@ -4,7 +4,12 @@ import B4Style from '../../styles/Bloco4.module.css'
 function Bloco4() {
     return (
         <div>
-            <div className={B4Style.bloco4background}></div>
+            <div className={B4Style.bloco4background}>
+                <p className={B4Style.goldGradient}>E OS CLÁSSICOS PODEM MUDAR A SUA VIDA – EM TODOS OS SENTIDOS.</p>
+                <p className={B4Style.lightText}>Todavia, surge uma pergunta nítida agora: </p>
+                <p className={B4Style.heavyText}>“Sim, mas na real, <span className={B4Style.blockText}>o que eu ganho na minha vida</span> aprendendo a ler bem os clássicos?”</p>
+
+            </div>
         </div>
     )
 }
