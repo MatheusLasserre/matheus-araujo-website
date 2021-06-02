cc5e4c6756ae5b0d77c97bd7845feed93cd87f5e
Matheus Lasserre <matheuslasserre3@gmail.com>
2021-06-02 03:29:37 -0300

    Bloco 8 finalizado

--- components/ROC/Bloco8.js
+++ components/ROC/Bloco8.js
@@ -16,10 +16,12 @@ function Bloco8() {
                     />
                 </div>
                 <p className={B8Style.goldHead}>O QUE ESTÃO DIZENDO SOBRE A METODOLOGIA:</p>
+                
                 <div className={B8Style.testimonialBox}>
                     <div className={B8Style.testimonialNameBox}>
                         <p className={B8Style.testimonialName}>Guilherme Cunha de Carvalho</p>
                     </div>
+                    <div className={B8Style.testimonialBoxDivider}></div>
                     <p className={B8Style.testimonialText}>A abordagem do Matheus, de compreender os clássicos como espelho, quem eu sou, e como janela, quem eu posso ser ou como os outros são, reaqueceu a minha paixão pela literatura. Ele promove a literatura como ela realmente é: uma experiência existencial que enriquece a nossa vida.</p>
                 </div>
                 <div className={B8Style.decorationSymbol}>
