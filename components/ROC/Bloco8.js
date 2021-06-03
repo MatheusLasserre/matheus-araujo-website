c490f3f37afc3d924c8586fb432b50b09fcded77
Matheus Lasserre <matheuslasserre3@gmail.com>
2021-06-02 23:00:30 -0300

    empilhamento de depoimentos

--- components/ROC/Bloco8.js
+++ components/ROC/Bloco8.js
@@ -27,21 +27,21 @@ function Bloco8() {
 
                 <div className={B8Style.testimonialBox}>
                 <div className={B8Style.testimonialNameBox}>
-                        <p className={B8Style.testimonialName}>Guilherme Cunha de Carvalho</p>
+                        <p className={B8Style.testimonialName}>Jaciara Pires</p>
                     </div>
                     <div className={B8Style.testimonialBoxDivider}></div>
-                    <p className={B8Style.testimonialText}>A abordagem do Matheus, de compreender os clássicos como espelho, quem eu sou, e como janela, quem eu posso ser ou como os outros são, reaqueceu a minha paixão pela literatura. Ele promove a literatura como ela realmente é: uma experiência existencial que enriquece a nossa vida.</p>
+                    <p className={B8Style.testimonialText}>Com a sua condução e orientação, as aulas e as leituras nos ensinam a ler as histórias e também os personagens, ajudando-nos a nos perceber em nossa própria realidade. Os clássicos são atemporais e são experiências que talvez nunca viveríamos se não fosse através dos livros.</p>
                 </div>
 
                 <div className={B8Style.testimonialBox}>
                 <div className={B8Style.testimonialNameBox}>
-                        <p className={B8Style.testimonialName}>Guilherme Cunha de Carvalho</p>
+                        <p className={B8Style.testimonialName}>Mariana Salustiano</p>
                     </div>
                     <div className={B8Style.testimonialBoxDivider}></div>
-                    <p className={B8Style.testimonialText}>A abordagem do Matheus, de compreender os clássicos como espelho, quem eu sou, e como janela, quem eu posso ser ou como os outros são, reaqueceu a minha paixão pela literatura. Ele promove a literatura como ela realmente é: uma experiência existencial que enriquece a nossa vida.</p>
+                    <p className={B8Style.testimonialText}>A literatura é apresentada pelo Matheus sob uma abordagem diferente de tudo que já vi. Conseguimos compreender que literatura é muito mais que simplesmente um livro antigo que um estranho escreveu. Não! Literatura é autoconhecimento, é evolução intelectual, literatura é um dos poucos meios que temos para nos conhecermos, nos tornarmos pessoas melhores, para enxergarmos todas as dimensões da vida e com isso melhorarmos nossa realidade de vida.</p>
                 </div>
 
-                
+
                 </div>
                 <div className={B8Style.decorationSymbol}>
                         <Image
