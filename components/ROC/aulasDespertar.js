6e7a4fec63376df095f14dd2e2c4262cf95731f6
Matheus Lasserre <matheuslasserre3@gmail.com>
2021-06-04 04:03:13 -0300

    Página das aulas

---
+++ components/ROC/aulasDespertar.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import adStyle from '../../styles/aulasDespertar.module.css'
+
+function aulasDespertar(props) {
+    return (
+        <div>
+            <div className={adStyle.contentWrap}>
+                <p className={adStyle.title}>{props.titlee}</p>
+                <p className={adStyle.subtitle}>{props.subtitle}</p>
+                <div className={adStyle.videoWrap}>
+                <iframe
+                    src={props.src}
+                    frameborder="0"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                    allowfullscreen
+                    title="video"
+                    width={1280}
+                    height={720}
+                    className={adStyle.videoWrap}
+                />
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default aulasDespertar
---
+++ pages/o-despertar-aulas.js
@@ -0,0 +1,75 @@
+import React from 'react'
+import odaStyle from '../styles/o-despertar-aulas.module.css'
+import Image from 'next/image'
+import AulasDespertar from '../components/ROC/aulasDespertar.js'
+
+function oDespertarAulas() {
+
+
+
+    return (
+        <div>
+            <div className={odaStyle.contentWrap}>
+                <div className={odaStyle.overflowControl}>
+                    <div className={odaStyle.background}>
+                        <div className={odaStyle.background2}>
+                            <div className={odaStyle.background3}>
+                                <div className={odaStyle.decorationSymbol}>
+                                    <Image
+                                        src="/images/ROC/ROC-SMB-3.png"
+                                        alt="Decoration Symbol"
+                                        width={32}
+                                        height={32}
+                                        className={odaStyle.decorationSymbol}
+                                    />
+                                </div>
+
+                                <div className={odaStyle.logo}>
+                                <Image
+                                        src="/images/ROC/ROC-LOGO-DESPERTAR.png"
+                                        alt="Despertar logo"
+                                        width={289}
+                                        height={70}
+                                        className={odaStyle.decorationSymbol}
+                                    />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            <div className={odaStyle.videosBackground}>
+            <div className={odaStyle.videosWrap}>
+
+                <div className={odaStyle.videoWrap}>
+                    <AulasDespertar
+                        src="https://www.youtube.com/embed/2nHMUg9u6vU"
+                        titlee="AULA 1"
+                        subtitle="Como a Escola Te Enganou"
+                    />
+                </div>
+
+                <div className={odaStyle.videoWrap}>
+                    <AulasDespertar
+                        src="https://www.youtube.com/embed/OIUbErA7wy0"
+                        titlee="AULA 2"
+                        subtitle="O Que Realmente é Literatura"
+                    />
+                </div>
+
+                <div className={odaStyle.videoWrap}>
+                    <AulasDespertar
+                        src="https://www.youtube.com/embed/9CI9135p6m0"
+                        titlee="AULA 3"
+                        subtitle="Os Clássicos Podem Mudar a Sua Vida"
+                    />
+                </div>
+
+            </div>
+            </div>
+
+            </div>
+        </div>
+    )
+}
+
+export default oDespertarAulas
