5bf8b16cb964a39063aa86c10016fc1824e211d6
Matheus Lasserre <matheuslasserre3@gmail.com>
2021-06-30 19:23:50 -0300

    Adicionado CountDown

--- components/ROC/Bloco9.js
+++ components/ROC/Bloco9.js
@@ -3,8 +3,22 @@ import B9Style from '../../styles/Bloco9.module.css'
 import Image from 'next/image'
 import FAQ from './FAQ.js'
 import Button from './Button'
+import Countdown from './countdown'
 
 function Bloco9() {
+    var countDownDate = new Date("Jul 1, 2021 00:00:00").getTime();
+
+    var now = new Date().getTime();
+
+    var distance = countDownDate - now;
+
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    const hoursMinSecs = {hours:hours, minutes: minutes, seconds: seconds}
+
     return (
         <div>
             <div className={B9Style.flexContainer}>
@@ -56,7 +70,7 @@ function Bloco9() {
                     answer="O acesso às aulas e a todos os bônus será vitalício – apenas para os que entrarem nessa primeira turma."
                 />
                 <div className={B9Style.spacer}></div>
-                <div className={B9Style.desktopHide}>
+                <div>
                 <Button
                     txt="QUERO GARANTIR A MINHA VAGA"
                 />
@@ -69,9 +83,7 @@ function Bloco9() {
                 </div>
                 <p className={B9Style.finalText}>Matheus Araújo é escritor, professor de Literatura, com mais de 5 (cinco) anos de experiência no ensino literário e foi aluno da Oficina Literária de Raimundo Carrero, uma das mais importantes oficinas literárias do Brasil.</p>
                 <p className={B9Style.finalText2}>Criador do Curso Online de Literatura, conta com mais de 150 alunos, atuando diariamente com escrita literária, leitura dos clássicos, mentorias e consultorias para aspirantes a escritores, professores de literatura e toda e qualquer pessoa que queira se aprofundar no mundo dos clássicos.</p>
-                <Button
-                    txt="QUERO GARANTIR A MINHA VAGA"
-                />
+                <Countdown hoursMinSecs={hoursMinSecs}/>
             </div>
             </div>
         </div>
---
+++ components/ROC/countdown.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import CStyle from '../../styles/countdown.module.css'
+
+function countdown({hoursMinSecs}) {
+
+    const { hours = 0, minutes = 0, seconds = 60 } = hoursMinSecs;
+    const [[hrs, mins, secs], setTime] = React.useState([hours, minutes, seconds]);
+    
+
+    const tick = () => {
+   
+        if (hrs === 0 && mins === 0 && secs === 0) 
+            reset()
+        else if (mins === 0 && secs === 0) {
+            setTime([hrs - 1, 59, 59]);
+        } else if (secs === 0) {
+            setTime([hrs, mins - 1, 59]);
+        } else {
+            setTime([hrs, mins, secs - 1]);
+        }
+    };
+
+
+    const reset = () => setTime([parseInt(hours), parseInt(minutes), parseInt(seconds)]);
+
+    
+    React.useEffect(() => {
+        const timerId = setInterval(() => tick(), 1000);
+        return () => clearInterval(timerId);
+    });
+
+    return (
+        <div className={CStyle.countdownFont}>
+             <p className={CStyle.timeFont}>{`${hrs.toString().padStart(2, '0')} :`}<br/><span className={CStyle.dateFont}>DIAS</span></p>
+             <p className={CStyle.timeFont}>{`${mins.toString().padStart(2, '0')} :`}<br/><span className={CStyle.dateFont2}>Minutos</span></p>
+             <p className={CStyle.timeFont}>{`${secs.toString().padStart(2, '0')}`}&nbsp;&nbsp;<br/><span className={CStyle.dateFont3}>Segundos</span></p>
+        </div>
+    )
+}
+
+export default countdown
